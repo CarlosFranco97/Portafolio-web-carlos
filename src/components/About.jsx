09e405087b38979648753fae2a701c6f6e3c387f
carlosfranco97 <@carlosfranco63.cf@gmail.com>
2024-05-12 14:43:47 -0400

    Se eliminan componentes y se modifica el titulo de letra del componente Aboout

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ export const About = () => {
         <div className='flex flex-col justify-center items-center w-full h-full'>
             <div className="max-w-[1000px] w-full flex">
                 <div className='text-right pb-8 pl-4'>
-                    <p className='text-3xl font-bold inline border-b-4 border-red-300'>Acerca de mí</p>
+                    <p className='text-4xl font-bold inline border-b-4 border-red-300'>Acerca de mí</p>
                 </div>
             </div>
             <div className='max-w-[1000px] w-full grid md:grid-cols-2 gap-8 px-4'>
